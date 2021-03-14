031e25a1c2c22293653c1af3de47edd96e7f112b
GitHub Action <action@github.com>
2021-03-14 10:00:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/veroz1/xposedtemplatepluginforas/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/veroz1/xposedtemplatepluginforas/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.veroz1.xposedtemplatepluginforas.listeners
 
+import com.github.veroz1.xposedtemplatepluginforas.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/veroz1/xposedtemplatepluginforas/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.veroz1.xposedtemplatepluginforas.services
+
+import com.github.veroz1.xposedtemplatepluginforas.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
